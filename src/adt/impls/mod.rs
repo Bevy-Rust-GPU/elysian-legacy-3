58556d329c2e61cc67074725e045dfcc9c6b0479
Josh Palmer <1253239+Shfty@users.noreply.github.com>
2023-06-25 19:45:41 +0100

    Remove invalid/unused newtype typeclass instances

--- src/adt/impls/mod.rs
+++ src/adt/impls/mod.rs
@@ -1,23 +1,9 @@
 //! Function implementations for the ADT
 
-mod apply;
-mod chain;
 mod fmap;
 mod foldl;
 mod foldr;
-mod mappend;
-mod mconcat;
-mod mempty;
-mod pure;
-mod r#return;
 
-pub use apply::*;
-pub use chain::*;
 pub use fmap::*;
 pub use foldl::*;
 pub use foldr::*;
-pub use mappend::*;
-pub use mconcat::*;
-pub use mempty::*;
-pub use pure::*;
-pub use r#return::*;
